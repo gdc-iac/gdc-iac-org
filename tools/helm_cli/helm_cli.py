03c392a0deda5e64daeff873a2c67665d09382f1
silviudobrica <silviudobrica@gmail.com>
2026-02-25 12:52:16 +0100

    Update helm_cli.py

--- tools/helm_cli/helm_cli.py
+++ tools/helm_cli/helm_cli.py
@@ -11,7 +11,8 @@ import logging
 import sys
 import yaml
 from collections import defaultdict
-from typing import List, Optional, Union
+from typing import List, Optional
+import tempfile
 import subprocess
 
 RESOURCE_TYPES = defaultdict(lambda: {
@@ -55,10 +56,7 @@ def resource_config(resource_type: str, obj: dict, parent: Optional[dict] = None
     
 
 def call_helm(action: str, resource_type: str, obj: dict, parent: Optional[dict] = None) -> None:
-    if parent:
-        logging.debug(f"call_helm {action} {resource_type}/{parent.get('name', '')}/{obj}")
-    else:
-        logging.debug(f"call_helm {action} {resource_type}/{obj}")
+    release_name = f"{resource_type}-{obj.get('name','root')}"
     try:
         with tempfile.NamedTemporaryFile(mode="w", suffix=".yaml") as tmp:
             values_yaml = yaml.safe_dump(resource_config(resource_type, obj, parent))
@@ -81,7 +79,7 @@ def call_helm(action: str, resource_type: str, obj: dict, parent: Optional[dict]
             logging.error(f"Error: Helm not found or could not be executed. {e}")
 
 
-def process_type(action: str, type_path: str, resource_type: str, type_tree: Union[dict, type], config: dict, dry_run: bool, parent: Optional[dict] = None) -> None:
+def process_type(action: str, type_path: str, resource_type: str, type_tree: dict | type, config: dict, dry_run: bool, parent: Optional[dict] = None) -> None:
     logging.debug(f"process_type {type_path}/{resource_type}")
     if resource_type not in config:
         return
