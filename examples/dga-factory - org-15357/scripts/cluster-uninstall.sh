80c8954cf581ea8229c2d69c7481c88f3737195f
Michał Ajduk <majduk@google.com>
2026-03-31 07:08:23 +0000

    Clean up examples

--- examples/dga-factory - org-15357/scripts/cluster-uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export SCRIPTS_DIR=$( readlink -e $(dirname $0))
-source ${SCRIPTS_DIR:?}/config.sh
-
-helm uninstall --namespace=${IAC_PROJECT:?} --kubeconfig=$USER_CLUSTER_KUBECONFIG $@
--- examples/dga-factory - org-15357/scripts/status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SCRIPTS_DIR=$( readlink -e $(dirname $0))
-source ${SCRIPTS_DIR:?}/config.sh
-
-${HELM_CLI:?} list --namespace=${IAC_PROJECT:?} --kubeconfig=$GLOBAL_API_KUBECONFIG -a
-${HELM_CLI:?} list --namespace=${IAC_PROJECT:?} --kubeconfig=$ZONE_KUBECONFIG -a
-${HELM_CLI:?} list --namespace=${IAC_PROJECT:?} --kubeconfig=$USER_CLUSTER_KUBECONFIG -a
